17ef8cecd99a6ee690d222fa72d7579d1031a4ad
danilo-uea <dsf.eng17@uea.edu.br>
2022-03-02 14:18:49 -0400

    Adicionando Typescript

---
+++ AppMobile/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text>
+        Hello Word!
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default App;
