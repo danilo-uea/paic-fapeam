a4593b552d0ba7887a21847ac66731d3893c8cff
Danilo Frazão <dsf.eng17@uea.edu.br>
2022-03-12 14:36:39 -0400

    bluetooth-ble

--- AppMobile/App.tsx
+++ AppMobile/App.tsx
@@ -1,17 +1,155 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {BleManager, Device} from 'react-native-ble-plx';
+import base64 from 'react-native-base64';
 
 import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
+  Button,
+  PermissionsAndroid,
+  LogBox,
 } from 'react-native';
 
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+const BLTManager = new BleManager();
+
+const SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+
+const MESSAGE_UUID = '6d68efe5-04b6-4a85-abc4-c2670b7bf7fd';
+
 const App = () => {
+  //Is a device connected?
+  const [isConnected, setIsConnected] = useState(false);
+
+  //What device is connected?
+  const [connectedDevice, setConnectedDevice] = useState<Device>();
+
+  const [message, setMessage] = useState('Esperando dados');
+
+  // Scans availbale BLT Devices and then call connectDevice
+  async function scanDevices() {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Permission Localisation Bluetooth',
+        message: 'Requirement for Bluetooth',
+        buttonNeutral: 'Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    ).then(answere => {
+      console.log('scanning');
+      // display the Activityindicator
+
+      BLTManager.startDeviceScan(null, null, (error, scannedDevice) => {
+        if (error) {
+          console.warn(error);
+        }
+                
+        if (scannedDevice && scannedDevice.name == 'ESP32') {
+          BLTManager.stopDeviceScan();
+          console.log('Conectou');
+          connectDevice(scannedDevice);
+        }
+      });
+
+      // stop scanning devices after 5 seconds
+      setTimeout(() => {
+        BLTManager.stopDeviceScan();
+        console.log('Parou');
+      }, 5000);
+    });
+  }
+
+  async function connectDevice(device: Device) 
+  {
+    console.log('connecting to Device:', device.name);
+
+    device
+      .connect()
+      .then(device => {
+        setConnectedDevice(device);
+        setIsConnected(true);
+        return device.discoverAllServicesAndCharacteristics();
+      })
+      .then(device => {
+        //  Set what to do when DC is detected
+        BLTManager.onDeviceDisconnected(device.id, (error, device) => {
+          console.log('Device DC');
+          setIsConnected(false);
+        });
+
+        //Message
+        // device
+        //   .readCharacteristicForService(SERVICE_UUID, MESSAGE_UUID)
+        //   .then(valenc => {
+        //     setMessage(base64.decode(valenc?.value));
+        //   });
+
+        //Message
+        device.monitorCharacteristicForService(
+          SERVICE_UUID,
+          MESSAGE_UUID,
+          (error, characteristic) => {
+            if (characteristic?.value != null) {
+              setMessage(base64.decode(characteristic?.value));
+              console.log(base64.decode(characteristic?.value));
+            }
+          },
+          'messagetransaction',
+        );
+      })
+  }
+
+  // handle the device disconnection (poorly)
+  async function disconnectDevice() {
+    console.log('Disconnecting start');
+
+    if (connectedDevice != null) {
+      const isDeviceConnected = await connectedDevice.isConnected();
+      if (isDeviceConnected) {
+        BLTManager.cancelTransaction('messagetransaction');
+        BLTManager.cancelTransaction('nightmodetransaction');
+
+        BLTManager.cancelDeviceConnection(connectedDevice.id).then(() =>
+          console.log('DC completed'),
+        );
+      }
+
+      const connectionStatus = await connectedDevice.isConnected();
+      if (!connectionStatus) {
+        setIsConnected(false);
+      }
+    }
+  }
+
   return (
     <View style={styles.sectionContainer}>
       <Text>
-        
+        {message}
       </Text>
+      <TouchableOpacity style={{width: 120}}>
+        {!isConnected ? (
+            <Button
+              title="Connect"
+              onPress={() => {
+                scanDevices();
+              }}
+              disabled={false}
+            />
+          ) : (
+            <Button
+              title="Disonnect"
+              onPress={() => {
+                disconnectDevice();
+              }}
+              disabled={false}
+            />
+          )}
+      </TouchableOpacity>
     </View>
   );
 };
